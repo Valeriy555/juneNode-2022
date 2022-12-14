74f45ab9316b87a882c78e8cc708f7bcfe3a6037
Valery555 <sokolchenkov@gmail.com>
2022-12-14 17:58:45 +0200

    start express

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const userDb = require('./dataBase/users')
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 
 app.get('/users', (req, res) => {
     console.log('USERS ENDPOINT');
@@ -37,10 +40,33 @@ app.get('/userDb/0', (req, res) => { // вызываем первого юзер
     res.json(userDb[0]);
 });
 
-app.get('/userDb/1', (req, res) => { // вызываем второго юзера
-    res.json(userDb[1]);
+app.get('/userDb/:id', (req, res) => { // вызываем юзеров динамично
+    console.log(req.params);
+    console.log(req.query);
+
+    const {id} = req.params;
+
+    res.json(userDb[id]);
 });
 
+app.post('/userDb',(req, res) => {
+
+    const userInfo = req.body;
+
+
+    userDb.push(userInfo)
+
+    res.status(201).json('Created')
+})
+
+app.put('/userDb/:id', (req,res) => {
+    const newUserInfo = req.body;
+    const userId = req.params.id;
+
+    userDb[userId]=newUserInfo;
+
+    res.json('Updated')
+})
 
 app.listen(5000, () => {
     console.log('Server listen 5000');
