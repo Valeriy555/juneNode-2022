ad23f1dc78f832b11a36adbba9cd3cb7bd1e0384
Valery555 <sokolchenkov@gmail.com>
2023-01-08 17:44:30 +0200

    logout

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -6,15 +6,15 @@ const {WELCOME} = require("../config/email-action.enum");
 module.exports = {
     login: async (req, res, next) => {
         try {
-            const { user, body } = req;
+            const {user, body} = req;
 
-            await emailService.sendEmail('valeragol0506@gmail.com', WELCOME, {userName:user.name});
+            await emailService.sendEmail('valeragol0506@gmail.com', WELCOME, {userName: user.name});
 
             await oauthService.comparePasswords(user.password, body.password);
 
-            const tokenPair = oauthService.generateAccessTokenPair({ id: user._id });
+            const tokenPair = oauthService.generateAccessTokenPair({id: user._id});
 
-            await OAuth.create({ ...tokenPair, _user_id: user._id })
+            await OAuth.create({...tokenPair, _user_id: user._id})
 
             res.json({
                 user,
@@ -27,17 +27,43 @@ module.exports = {
 
     refresh: async (req, res, next) => {
         try {
-            const { refreshToken, _user_id } = req.tokenInfo;
+            const {refreshToken, _user_id} = req.tokenInfo;
 
-            await OAuth.deleteOne({ refreshToken });
+            await OAuth.deleteOne({refreshToken});
 
-            const tokenPair = oauthService.generateAccessTokenPair({ id: _user_id });
+            const tokenPair = oauthService.generateAccessTokenPair({id: _user_id});
 
-            await OAuth.create({ ...tokenPair, _user_id })
+            await OAuth.create({...tokenPair, _user_id})
 
             res.status(201).json(tokenPair);
         } catch (e) {
             next(e);
         }
-    }
+    },
+
+    logout: async (req, res, next) => {
+        try {
+            const {accessToken} = req.tokenInfo;
+
+            await OAuth.deleteOne({accessToken});
+
+            res.sendStatus(204);
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    logoutAll: async (req, res, next) => {
+        try {
+            const {_user_id} = req.tokenInfo;
+
+            await OAuth.deleteMany({_user_id});
+
+            res.sendStatus(204);
+        } catch (e) {
+            next(e);
+        }
+    },
+
+
 }
--- router/auth.router.js
+++ router/auth.router.js
@@ -1,17 +1,24 @@
 const router = require('express').Router();
 
 const controller = require('../controller/auth.controller');
-const middleware = require('../middleware/auth.middleware');
 const userMiddleware = require('../middleware/user.middleware');
 const authMiddleware = require('../middleware/auth.middleware');
 
 router.post('/login',
-    middleware.isBodyValid,
+    authMiddleware.isBodyValid,
     userMiddleware.getUserDynamically('email'),
     controller.login);
 
 router.post('/refresh',
     authMiddleware.checkRefreshToken,
     controller.refresh);
+
+router.post('/logout',
+    authMiddleware.checkAccessToken,
+    controller.logout);
+
+router.post('/logoutAll',
+    authMiddleware.checkAccessToken,
+    controller.logoutAll);
 
 module.exports = router;
